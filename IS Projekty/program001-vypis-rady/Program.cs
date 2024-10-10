70dd0f231cf11f5e667abb8a0928f23c51ce5cc7
Eliska Holubova <eliska.holubova1@tul.cz>
2024-10-10 13:54:47 +0200

    # Vstup hodnot a jejich výpis

--- IS Projekty/program001-vypis-rady/Program.cs
+++ IS Projekty/program001-vypis-rady/Program.cs
@@ -11,12 +11,48 @@ class Program {
             Console.WriteLine("**********Výpis řady***********");
             Console.WriteLine("*******************************");
             Console.WriteLine("*******Eliška Holubová*********");
-            Console.WriteLine("*******************************");
+            Console.WriteLine("*******************************\n");
             Console.WriteLine();            // prázdný řádek takto a nebo pomocí \n za poslední řádek nad tímto
             
-            // vstup od uživatele - TO-DO
+            // vstup od uživatele - špatná varianta
+            // Console.Write("Zadejte první číslo řady: ");
+            // int first = int.Parse(Console.ReadLine());
+
+
+            // logika pro výpis řady - lepší varianta
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
+
+              Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu poslední číslo řady (celé číslo): ");
+            }
+
+              Console.Write("Zadejte třetí číslo řady (celé číslo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte diferenci (celé číslo): ");
+            }
+
+
+            // výpis uživatelského vstupu
+            Console.WriteLine();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("První číslo řady: {0}", first);
+            Console.WriteLine("Poslední číslo řady: {0}", last);
+            Console.WriteLine("Diference {0}", step);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
             // logika pro výpis řady - TO-DO
-            // opakování programu
+
             Console.WriteLine("Pro opakování programu stiskněte klávesu a");
             again = Console.ReadLine();
 
