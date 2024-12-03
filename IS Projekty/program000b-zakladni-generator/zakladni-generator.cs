ed924da6bcc0548d368854fe1cf352aef1ed65f5
Eliska Holubova <eliska.holubova1@tul.cz>
2024-12-03 19:35:44 +0100

    #7 Analýza textu

--- IS Projekty/program000b-zakladni-generator/zakladni-generator.cs
+++ IS Projekty/program000b-zakladni-generator/zakladni-generator.cs
@@ -1,41 +1,38 @@
-class Program {
+using System.Security.Authentication.ExtendedProtection;
 
-    static void Main() {
-        
-        //program se začne opakovat po stisku klávesy "a"
-        string again = "a";             // operátor = je přiřazení hodnoty
-        while(again == "a") {          // == je porovnávání hodnot
-            Console.Clear();
-            Console.WriteLine("********************************************************");
-            Console.WriteLine("************ Generátor pseudonáhoných čísel ************");
-            Console.WriteLine("********************************************************");
-            Console.WriteLine("******************** Eliška Holubová *******************");
-            Console.WriteLine("********************************************************n\n");
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("******* Generátor pseudonáhodných čísel *******");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("************** Eliška Holubová ****************");
+            Console.WriteLine("***********************************************\n\n");
             Console.WriteLine();
 
-            // Vstup od uživatele TO-DO - správná verze
+            
             Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
             int n;
             while(!int.TryParse(Console.ReadLine(), out n)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte znovu počet čísel (celé číslo): ");
-            }     
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
 
             Console.Write("Zadejte dolní mez (celé číslo): ");
             int dm;
             while(!int.TryParse(Console.ReadLine(), out dm)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
-            }     
+            }
 
             Console.Write("Zadejte horní mez (celé číslo): ");
             int hm;
             while(!int.TryParse(Console.ReadLine(), out hm)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
-            }     
+            }
 
-            Console.WriteLine("\n\n============================================================");
-            Console.WriteLine("Uživatelský vstup:");
+            Console.WriteLine("\n\n====================");
+            Console.WriteLine("Uživatelský vstup: ");
             Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
-            Console.WriteLine("============================================================");
+            Console.WriteLine("====================\n\n");
 
             // deklarace pole
             int[] myArray = new int[n];
@@ -46,16 +43,11 @@ class Program {
             Console.WriteLine("Náhodná čísla: ");
 
             for(int i=0; i<n; i++) {
-                myArray[i] = randomNumber.Next(dm, hm);
+                myArray[i] = randomNumber.Next(dm, hm+1);
                 Console.Write("{0}; ", myArray[i]);
             }
             
-
             // Opakování programu
             Console.WriteLine("\n\nPro opakování programu stiskněte klávesu a");
             again = Console.ReadLine();
         }
-
-    }
-
-}
---
+++ IS Projekty/program006-analyza-textu/Program.cs
@@ -0,0 +1,52 @@
+﻿// chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("******************************");
+            Console.WriteLine("******* Analýza textu *******");
+            Console.WriteLine("******************************");
+            Console.WriteLine("****** Eliška Holubová *******");
+            Console.WriteLine("******************************\n\n");
+            Console.WriteLine();
+
+            Console.Write("\nZadejte text pro analýzu: ");
+            string myText = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine(myText);
+            Console.WriteLine(myText[0]);
+            Console.WriteLine(myText.Length);
+            Console.WriteLine(myText[myText.Length-1]);
+
+            string samohlasky = "aáeéěiíoóuůúyý";
+            string souhlasky = "bcčdďfghjklmnňpqrřsštťvwxzž";
+            string cislice = "0123456789";
+            
+            int pocetSamohlasek = 0;
+            int pocetSouhlasek = 0;
+            int pocetCislic = 0;
+            int pocetOstatnich = 0;
+
+            foreach(char znak in myText) {
+                if(souhlasky.Contains(znak)) {
+                    pocetSouhlasek++;
+                }
+                else if(samohlasky.Contains(znak)) {
+                    pocetSamohlasek++;
+                }
+                else if(cislice.Contains(znak)) {
+                    pocetCislic++;
+                }
+                else
+                    pocetOstatnich++;
+            }
+
+            Console.WriteLine("\n\nPočet samohlásek: {0}", pocetSamohlasek);
+            Console.WriteLine("Počet souhlásek: {0}", pocetSouhlasek);
+            Console.WriteLine("Počet číslic: {0}", pocetCislic);
+            Console.WriteLine("Počet ostatních znaků: {0}", pocetOstatnich);
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
