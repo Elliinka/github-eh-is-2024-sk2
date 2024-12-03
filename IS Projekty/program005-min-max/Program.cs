51ff3bce4b868e117f86cdebde46f777f3a10f60
Eliska Holubova <eliska.holubova1@tul.cz>
2024-12-03 19:20:46 +0100

    #6 Min Max (Úkol 5)

---
+++ IS Projekty/program005-min-max/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+
+string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("******* Generátor pseudonáhodných čísel *******");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("************* Eliška Holubová *****************");
+            Console.WriteLine("***********************************************\n\n");
+            Console.WriteLine();
+
+            
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
+
+            Console.Write("Zadejte dolní mez (celé číslo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
+            }
+
+            Console.Write("Zadejte horní mez (celé číslo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
+            }
+
+            Console.WriteLine("\n\n====================");
+            Console.WriteLine("Uživatelský vstup: ");
+            Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
+            Console.WriteLine("====================\n\n");
+
+            // deklarace pole
+            int[] myArray = new int[n];
+
+            // příprava pro generování náhodných čísel
+            Random randomNumber = new Random();
+
+            Console.WriteLine("Náhodná čísla: ");
+
+            for(int i=0; i<n; i++) {
+                myArray[i] = randomNumber.Next(dm, hm+1);
+                Console.Write("{0}; ", myArray[i]);
+            }
+
+            int max = myArray[0];
+            int min = myArray[0];
+            int posMax = 0;
+            int posMin = 0;
+
+            for(int i=1 ; i < n; i++) {
+                if(myArray[i] > max) {
+                    max = myArray[i];
+                    posMax = i;
+                }
+
+                if(myArray[i] < min) {
+                    min = myArray[i];
+                    posMin = i;
+                }
+                
+            }
+
+
+
+            Console.WriteLine("\n\nMaximum = {0}; První pozice max v poli: {1}", max, posMax);
+            Console.WriteLine("Minimum = {0}; První pozice min v poli: {1}", min, posMin);
+            
+
+            // Opakování programu
+            Console.WriteLine("\n\nPro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
